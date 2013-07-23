09dde6414575cd5b97182ef51c6bdee4052ec4da
Kevon Cheung <kevon.cheung@gmail.com>
2013-07-23 17:20:45 -0400

    Implemented basic function of checking deposit

--- scaffolding/js/atm.js
+++ scaffolding/js/atm.js
@@ -1,28 +1,27 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/GlobalEventHandlers.onload
 // The load event fires at the end of the document loading process.
-// At this point, all of the objects in the document are in the DOM, 
+// At this point, all of the objects in the document are in the DOM,
 // and all the images and sub-frames have finished loading.
 
 window.onload = function(){
 
-  // https://developer.mozilla.org/en-US/docs/Web/API/GlobalEventHandlers.onclick
-  // The click event is raised when the user clicks on an element. 
-
   document.getElementById("checkingDeposit").onclick = function(event){
-    // Any code you put in here will be run when the checkingDeposit button is clicked
+    var checkingAmount = document.getElementById('checkingAmount').value;
+    var checkingBalance = document.getElementById('checkingBalance');
+    var newBalance = +checkingBalance.innerText.replace("$","") + +checkingAmount; // place plus sign in front to turn it into integer
+    checkingBalance.innerHTML = '$' + newBalance;
   };
 
   document.getElementById("savingsDeposit").onclick = function(event){
-    // Any code you put in here will be run when the savingsDeposit button is clicked
+
   };
 
   document.getElementById("checkingWithdraw").onclick = function(event){
-    // Any code you put in here will be run when the checkingWithdraw button is clicked
+
   };
 
   document.getElementById("savingsWithdraw").onclick = function(event){
-    // Any code you put in here will be run when the savingsWithdraw button is clicked
+
   };
 
 };
-
