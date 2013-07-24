92e6cc370feec1b3c347d5d1225d054dd1728cff
nicklombardi <nicklombardi@gmail.com>
2013-07-24 07:53:50 -0400

    refactored cimpletely working on overdraft protection

--- scaffolding/js/atm.js
+++ scaffolding/js/atm.js
@@ -11,46 +11,56 @@ window.onload = function(){
   // https://developer.mozilla.org/en-US/docs/Web/API/GlobalEventHandlers.onclick
   // The click event is raised when the user clicks on an element.
 
+var checking = 0;
+var savings = 0;
+
+var checking_balance = +document.getElementById("checkingBalance").innerText.replace("$", "");
+
+var savings_balance = +document.getElementById("savingsBalance").innerText.replace("$", "");
+
+
+
   document.getElementById("checkingDeposit").onclick = function(event){
     // Any code you put in here will be run when the checkingDeposit button is clicked
-    var amount = document.getElementById("checkingAmount").value;
-    var balance = +document.getElementById("checkingBalance").innerText.replace("$", "") + +amount;
-
-    checkingBalance.innerHTML = "$" + balance;
+      var deposit = parseFloat(document.getElementById('checkingAmount').value);
+      checking = deposit += parseFloat(checking);
+      var checking_balance = document.getElementById("checkingBalance");
+     checking_balance.innerHTML = parseFloat(checking);
 
   };
 
   document.getElementById("savingsDeposit").onclick = function(event){
     // Any code you put in here will be run when the savingsDeposit button is clicked
-    var amount = document.getElementById("savingsAmount").value;
-    var balance = +document.getElementById("savingsBalance").innerText.replace("$", "") + +amount;
-
-    savingsBalance.innerHTML = "$" + balance;
+      var deposit = parseFloat(document.getElementById('savingsAmount').value);
+      savings = deposit += parseFloat(savings);
+      var savings_balance = document.getElementById("savingsBalance");
+     savings_balance.innerHTML = parseFloat(savings);
 
   };
 
   document.getElementById("checkingWithdraw").onclick = function(event){
     // Any code you put in here will be run when the checkingWithdraw button is clicked
-    var amount = document.getElementById("checkingAmount").value;
-    var balance = +document.getElementById("checkingBalance").innerText.replace("$", "") - +amount;
-    if (amount > (+document.getElementById("checkingBalance").innerText.replace("$", ""))) {
-      alert("Insufficient funds");
-      } else {
-        checkingBalance.innerHTML = "$" + balance;
+      var withdraw = parseFloat(document.getElementById('checkingAmount').value);
+
+      if ((withdraw > checking) && (withdraw < checking + savings)) {
+           alert("overdraft protection");
+          } else if (withdraw > checking) {
+           alert("insufficient funds");
+            } else {
+            checking = Math.abs(withdraw - parseFloat(checking));
+            var checking_text = document.getElementById("checkingBalance");
+            checking_text.innerHTML = parseFloat(checking);
     }
+   };
 
   };
 
   document.getElementById("savingsWithdraw").onclick = function(event){
     // Any code you put in here will be run when the savingsWithdraw button is clicked
-    var amount = document.getElementById("savingsAmount").value;
-    var balance = +document.getElementById("savingsBalance").innerText.replace("$", "") - +amount;
-    if (amount > (+document.getElementById("savingsBalance").innerText.replace("$", ""))) {
-      alert("Insufficient funds");
-      } else {
-    savingsBalance.innerHTML = "$" + balance;
-    }
-  };
+      var withdraw = parseFloat(document.getElementById('savingsAmount').value);
+      savings = parseFloat(savings) - withdraw;
+      var savings_balance = document.getElementById("savingsBalance");
+     savings_balance.innerHTML = parseFloat(savings);
 
 };
 
