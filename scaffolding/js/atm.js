667c3d465405ac00b44b4076ffa4c408bb0006db
Beryl <beryl.schragger@gmail.com>
2013-07-23 21:18:48 -0400

    Add functions for depositing into checking and savings.

--- scaffolding/js/atm.js
+++ scaffolding/js/atm.js
@@ -1,18 +1,31 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/GlobalEventHandlers.onload
 // The load event fires at the end of the document loading process.
-// At this point, all of the objects in the document are in the DOM, 
+// At this point, all of the objects in the document are in the DOM,
 // and all the images and sub-frames have finished loading.
+var cBalance = 0;
+var sBalance = 0;
 
 window.onload = function(){
 
+  var cBalance = document.getElementById("checkingBalance");
+  var sBalance = document.getElementById("savingsBalance");
+  var cBalanceValue = parseInt(cBalance.innerText.slice(1, 100));
+  var sBalanceValue = parseInt(sBalance.innerText.slice(1, 100));
+
   // https://developer.mozilla.org/en-US/docs/Web/API/GlobalEventHandlers.onclick
-  // The click event is raised when the user clicks on an element. 
+  // The click event is raised when the user clicks on an element.
 
   document.getElementById("checkingDeposit").onclick = function(event){
+    var increase = parseInt(document.getElementById("checkingAmount").value);
+    cBalanceValue += increase;
+    cBalance.innerText = "$" + cBalanceValue;
     // Any code you put in here will be run when the checkingDeposit button is clicked
   };
 
   document.getElementById("savingsDeposit").onclick = function(event){
+    var increase = parseInt(document.getElementById("savingsAmount").value);
+    sBalanceValue += increase;
+    sBalance.innerText = "$" + sBalanceValue;
     // Any code you put in here will be run when the savingsDeposit button is clicked
   };
 
