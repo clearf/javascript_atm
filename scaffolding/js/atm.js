cb280ebda86e919ba65902f8db7c7e9a68dd1555
alicem <alice.mottola@gmail.com>
2013-07-23 20:37:56 -0400

    Refactored to make DRY-er, but I think the code is worse on the whole

--- scaffolding/js/atm.js
+++ scaffolding/js/atm.js
@@ -7,72 +7,84 @@ window.onload = function() {
 
   // https://developer.mozilla.org/en-US/docs/Web/API/GlobalEventHandlers.onclick
   // The click event is raised when the user clicks on an element.
+
+  // SETUP
   var checkingBalBox = document.getElementById("checkingBalance");
-  var checking = 0;
+  var checking = {
+    amt: 0
+  };
 
   var savingsBalBox = document.getElementById("savingsBalance");
-  var savings = 0;
+  var savings = {
+    amt: 0
+  };
 
   checkingBalBox.style.background = "#f33";
   savingsBalBox.style.background = "#f33";
 
+  // DEPOSIT FUNCTIONS
   document.getElementById("checkingDeposit").onclick = function(event) {
-    checking += parseInt(document.getElementById("checkingAmount").value, 10);
-    checkingBalBox.innerText = "$" + checking;
-    if (checking > 0) {
+    checking["amt"] += parseInt(document.getElementById("checkingAmount").value, 10);
+    checkingBalBox.innerText = "$" + checking["amt"];
+    if (checking["amt"] > 0) {
       checkingBalBox.style.background = "#E3E3E3";
     }
   };
 
   document.getElementById("savingsDeposit").onclick = function(event) {
-    savings += parseInt(document.getElementById("savingsAmount").value, 10);
-    savingsBalBox.innerText = "$" + savings;
-    if (savings > 0) {
+    savings["amt"] += parseInt(document.getElementById("savingsAmount").value, 10);
+    savingsBalBox.innerText = "$" + savings["amt"];
+    if (savings["amt"] > 0) {
       savingsBalBox.style.background = "#E3E3E3";
     }
   };
 
+  // TOP-LEVEL WITHDRAWAL FUNCTIONS
   document.getElementById("checkingWithdraw").onclick = function(event) {
-    var newAmt = checking - parseInt(document.getElementById("checkingAmount").value, 10);
-    if (newAmt < 0) {
-      if (newAmt + savings >= 0) {
-        checking = 0;
-        savings += newAmt;
-        savingsBalBox.innerText = "$" + savings;
-        if (savings === 0) {
-          savingsBalBox.style.background = "#f33";
-        }
-      }
-    }
-    else {
-      checking = newAmt;
-    }
-    checkingBalBox.innerText = "$" + checking;
-    if (checking === 0) {
-      checkingBalBox.style.background = "#f33";
-    }
+    var withdrawalAmt = parseInt(document.getElementById("checkingAmount").value, 10);
+    withdraw(checking, savings, withdrawalAmt);
   };
 
   document.getElementById("savingsWithdraw").onclick = function(event) {
-    var newAmt = savings - parseInt(document.getElementById("savingsAmount").value, 10);
+    var withdrawalAmt = parseInt(document.getElementById("savingsAmount").value, 10);
+    withdraw(savings, checking, withdrawalAmt);
+  };
+
+  // ACCOUNT-BLIND WITHDRAWAL
+  function withdraw(accountToWithdrawFrom, backupAccount, withdrawalAmt) {
+    var newAmt = accountToWithdrawFrom["amt"] - withdrawalAmt;
     if (newAmt < 0) {
-      if (newAmt + checking >= 0) {
-        savings = 0;
-        checking += newAmt;
-        checkingBalBox.innerText = "$" + checking;
-        if (checking === 0) {
-          checkingBalBox.style.background = "#f33";
-        }
+      if (newAmt + backupAccount["amt"] >= 0) {
+        accountToWithdrawFrom["amt"] = 0;
+        backupAccount["amt"] += newAmt;
       }
     }
     else {
-      savings = newAmt;
+      accountToWithdrawFrom["amt"] = newAmt;
     }
-    savingsBalBox.innerText = "$" + savings;
-    if (savings === 0) {
+    resetBoxes();
+  }
+
+  // RESET BOXES HELPER METHOD
+  // This necessitates some unnecessary instructions for the "advantage"
+  // of DRYer code.
+  // All in all, I'd favor my less DRY previous commit.
+  function resetBoxes() {
+    checkingBalBox.innerText = "$" + checking["amt"];
+    if (checking["amt"] === 0) {
+      checkingBalBox.style.background = "#f33";
+    }
+    else {
+      checkingBalBox.style.background = "#E3E3E3";
+    }
+
+    savingsBalBox.innerText = "$" + savings["amt"];
+    if (savings["amt"] === 0) {
       savingsBalBox.style.background = "#f33";
     }
-  };
+    else {
+      savingsBalBox.style.background = "#E3E3E3";
+    }
+  }
 
 };
-
