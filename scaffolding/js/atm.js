f39d6ff7e5cde48703453218ecb6e9a620844d0f
Alina Guzman <alinochka8@gmail.com>
2013-07-23 22:33:07 -0400

    refactored withdrawl function

--- scaffolding/js/atm.js
+++ scaffolding/js/atm.js
@@ -7,35 +7,58 @@ window.onload = function(){
   // https://developer.mozilla.org/en-US/docs/Web/API/GlobalEventHandlers.onclick
   // The click event is raised when the user clicks on an element.
 
+   //Global variables of the balance elements to use to slightly dry up the code
+    var checkingBalance = parseInt(document.getElementById('checkingBalance').innerText.match(/\d+/)[0]);
+    var savingsBalance = parseInt(document.getElementById('savingsBalance').innerText.match(/\d+/)[0]);
+
+    //these statements make the bg color red on start since they are $0. Gotta figure out a way to dry it up
+    if (checkingBalance === 0){
+        document.getElementById('checkingBalance').style.backgroundColor = "red";
+    }
+    if (savingsBalance === 0){
+        document.getElementById('savingsBalance').style.backgroundColor = "red";
+    }
 
 
-
-  document.getElementById("checkingDeposit").onclick = function(event){
+    document.getElementById("checkingDeposit").onclick = function(event){
     // Any code you put in here will be run when the checkingDeposit button is clicked
-      var depositAmount = document.getElementById('checkingAmount').value;
-      var stringBalance = document.getElementById('checkingBalance').innerText.match(/\d+/)[0];
-      var checkingBalance = parseInt(stringBalance) + parseInt(depositAmount);
+      var depositAmount = parseInt(document.getElementById('checkingAmount').value);
+      checkingBalance = checkingBalance + depositAmount;
       document.getElementById('checkingBalance').innerText = "$" + checkingBalance;
+
+        //changes the bg color to grey when money is deposited
+        if (checkingBalance > 0){
+            document.getElementById('checkingBalance').style.backgroundColor = "#E3E3E3";
+        }
   };
 
   document.getElementById("savingsDeposit").onclick = function(event){
     // Any code you put in here will be run when the savingsDeposit button is clicked
-      var savingsAmount = document.getElementById('savingsAmount').value;
-      var stringBalance = document.getElementById('savingsBalance').innerText.match(/\d+/)[0];
-      var savingsBalance = parseInt(stringBalance) + parseInt(savingsAmount);
+      var savingsAmount = parseInt(document.getElementById('savingsAmount').value);
+      savingsBalance = savingsBalance + savingsAmount;
       document.getElementById('savingsBalance').innerText = "$" + savingsBalance;
+
+      //changes the bg color to grey when money is deposited
+      if (savingsBalance > 0){
+          document.getElementById('savingsBalance').style.backgroundColor = "#E3E3E3";
+      }
   };
 
   document.getElementById("checkingWithdraw").onclick = function(event){
     // Any code you put in here will be run when the checkingWithdraw button is clicked
-      var withdrawAmount = document.getElementById('checkingAmount').value;
-      var stringBalance = document.getElementById('checkingBalance').innerText.match(/\d+/)[0];
-      var checkingBalance = parseInt(stringBalance) - parseInt(withdrawAmount);
+      var withdrawAmount = parseInt(document.getElementById('checkingAmount').value);
+      var startingBalance = parseInt(document.getElementById('checkingBalance').innerText.match(/\d+/)[0]);
+      var checkingBalance = startingBalance - withdrawAmount;
 
+      //Stops from withdrawing below zero
       if (checkingBalance < 0){
-
       } else {
           document.getElementById('checkingBalance').innerText = "$" + checkingBalance;
+      }
+
+      //changes bg to red if balance goes to 0
+      if (checkingBalance === 0){
+          document.getElementById('checkingBalance').style.backgroundColor = "red";
       }
 
 
@@ -44,14 +67,19 @@ window.onload = function(){
   document.getElementById("savingsWithdraw").onclick = function(event){
     // Any code you put in here will be run when the savingsWithdraw button is clicked
       var withdrawAmount = document.getElementById('savingsAmount').value;
-      var stringBalance = document.getElementById('savingsBalance').innerText.match(/\d+/)[0];
-      var savingsBalance = parseInt(stringBalance) - parseInt(withdrawAmount);
-      if (savingsBalance < 0){
+      var startingBalance = document.getElementById('savingsBalance').innerText.match(/\d+/)[0];
+      var savingsBalance = parseInt(startingBalance) - parseInt(withdrawAmount);
 
+      //Stops from withdrawing below zero
+      if (savingsBalance < 0){
       } else {
           document.getElementById('savingsBalance').innerText = "$" + savingsBalance;
       }
-  };
 
+      //changes bg to red if balance goes to 0
+      if (savingsBalance === 0){
+          document.getElementById('savingsBalance').style.backgroundColor = "red";
+      }
+  };
 };
 
