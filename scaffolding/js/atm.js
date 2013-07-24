3a87fb4bc7824db6c3f772fee6804899e5a2dd1c
Christopher Wright <cmwright33@gmail.com>
2013-07-23 23:42:33 -0400

    finally got the two functions to add money, working on withdrawel now

--- scaffolding/js/atm.js
+++ scaffolding/js/atm.js
@@ -11,24 +11,27 @@ window.onload = function(){
   document.getElementById("checkingDeposit").onclick = function(event){
     // Any code you put in here will be run when the checkingDeposit button is clicked
     // the increment
-    document.getElementById('checkingBalance').textContent = ($ parseInt(document.getElementById('checkingBalance').textContent.match("[0-9]")) + document.getElementById('checkingAmount').value);
+  // document.getElementById('checkingAmount').value;
+   var balance = parseInt(document.getElementById('checkingBalance').textContent.match(/\d+/)) + parseInt(document.getElementById('checkingAmount').value);
+    document.getElementById("checkingBalance").textContent = '$' + balance;
   };
 
   document.getElementById("savingsDeposit").onclick = function(event){
     // Any code you put in here will be run when the savingsDeposit button is clicked
-    var balance = (parseInt(document.getElementById('savingsBalance').textContent[1]);
+    var balance = parseInt(document.getElementById('savingsBalance').textContent.match(/\d+/)) + parseInt(document.getElementById('savingsAmount').value);
+    document.getElementById("savingsBalance").textContent = '$' + balance;
   };
 
   document.getElementById("checkingWithdraw").onclick = function(event){
     // Any code you put in here will be run when the checkingWithdraw button is clicked
-    // Cannot go below 0
-    while (parseInt(document.getElementById('checkingBalance').textContent[1]);
+    // // Cannot go below 0
+    // while (parseInt(document.getElementById('checkingBalance').textContent[1]);
   };
 
   document.getElementById("savingsWithdraw").onclick = function(event){
     // Any code you put in here will be run when the savingsWithdraw button is clicked
     // Cannot go below 0
-    while (document.getElementById('checkingBalance').textContent[1])
+    // while (document.getElementById('checkingBalance').textContent[1])
   };
 
 };
