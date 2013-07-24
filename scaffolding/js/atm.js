e0eba2c4ffac2ea4037ceff6123ca67432144b5c
Jamal Powell <jamalkpowell@gmail.com>
2013-07-23 21:28:48 -0400

    got deposits working, figuring out saving them

--- scaffolding/js/atm.js
+++ scaffolding/js/atm.js
@@ -1,15 +1,25 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/GlobalEventHandlers.onload
 // The load event fires at the end of the document loading process.
-// At this point, all of the objects in the document are in the DOM, 
+// At this point, all of the objects in the document are in the DOM,
 // and all the images and sub-frames have finished loading.
 
 window.onload = function(){
 
   // https://developer.mozilla.org/en-US/docs/Web/API/GlobalEventHandlers.onclick
-  // The click event is raised when the user clicks on an element. 
+  // The click event is raised when the user clicks on an element.
+var deposit = 0;
 
   document.getElementById("checkingDeposit").onclick = function(event){
     // Any code you put in here will be run when the checkingDeposit button is clicked
+    // getting the value of deposit in an absolute # form.
+    var checking = parseFloat(document.getElementById("checkingAmount").value);
+    // Deposot is the "amount" put in. Adding this to the checking account
+    deposit = checking += parseFloat(deposit);
+    // making a variable out of the element ID checking balance.
+    var checkingBalance = (document.getElementById("checkingBalance"));
+    // making the balance above into the 'float' of deposit.
+    checkingBalance.innerHTML = parseFloat(deposit);
+    // how many goddamn SEMICOLONS
   };
 
   document.getElementById("savingsDeposit").onclick = function(event){
